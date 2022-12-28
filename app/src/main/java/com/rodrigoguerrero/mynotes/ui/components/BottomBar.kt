04e00ba0999f13872043132eb9b75382d1a7da20
Rodrigo Guerrero <rodrigo.guerreromulia@jumbo.com>
2022-12-28 12:17:26 +0100

    Migrate bottom app bar to material 3 bottom app bar.

--- app/src/main/java/com/rodrigoguerrero/mynotes/ui/components/BottomBar.kt
+++ app/src/main/java/com/rodrigoguerrero/mynotes/ui/components/BottomBar.kt
@@ -1,23 +1,20 @@
 package com.rodrigoguerrero.mynotes.ui.components
 
 import android.content.res.Configuration
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Brush
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.outlined.Brush
 import androidx.compose.material.icons.outlined.CheckBox
 import androidx.compose.material.icons.outlined.Image
 import androidx.compose.material.icons.outlined.Mic
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.rodrigoguerrero.mynotes.ui.theme.MyNotesTheme
@@ -30,48 +27,32 @@ fun BottomBar(
     onMicrophoneClicked: () -> Unit,
     onImageClicked: () -> Unit
 ) {
-    val interactionSource = remember { MutableInteractionSource() }
-
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(MyNotesTheme.color.surfaceVariant)
-            .padding(MyNotesTheme.padding.m),
-        horizontalArrangement = Arrangement.spacedBy(MyNotesTheme.padding.l)
-    ) {
-        Icon(
-            imageVector = Icons.Outlined.CheckBox,
-            contentDescription = null,
-            modifier = Modifier.clickable(
-                indication = null,
-                interactionSource = interactionSource
-            ) { onCheckboxClicked() }
-        )
-        Icon(
-            imageVector = Icons.Outlined.Brush,
-            contentDescription = null,
-            modifier = Modifier.clickable(
-                indication = null,
-                interactionSource = interactionSource
-            ) { onDrawingClicked() }
-        )
-        Icon(
-            imageVector = Icons.Outlined.Mic,
-            contentDescription = null,
-            modifier = Modifier.clickable(
-                indication = null,
-                interactionSource = interactionSource
-            ) { onMicrophoneClicked() }
-        )
-        Icon(
-            imageVector = Icons.Outlined.Image,
-            contentDescription = null,
-            modifier = Modifier.clickable(
-                indication = null,
-                interactionSource = interactionSource
-            ) { onImageClicked() }
-        )
-    }
+    BottomAppBar(
+        modifier = modifier,
+        actions = {
+            IconButton(onClick = onCheckboxClicked) {
+                Icon(imageVector = Icons.Outlined.CheckBox, contentDescription = null)
+            }
+            IconButton(onClick = onDrawingClicked) {
+                Icon(imageVector = Icons.Outlined.Brush, contentDescription = null)
+            }
+            IconButton(onClick = onMicrophoneClicked) {
+                Icon(imageVector = Icons.Outlined.Mic, contentDescription = null)
+            }
+            IconButton(onClick = onImageClicked) {
+                Icon(imageVector = Icons.Outlined.Image, contentDescription = null)
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { },
+                containerColor = MyNotesTheme.color.surfaceVariant,
+                elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+            ) {
+                Icon(Icons.Filled.Add, null)
+            }
+        }
+    )
 }
 
 @Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
